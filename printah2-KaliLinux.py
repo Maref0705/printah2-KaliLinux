a79deb6c4e9fd64410878e809dae18e01ebd191b
Maref0705 <60621806+Maref0705@users.noreply.github.com>
2020-03-27 23:25:42 +0700

    Add files via upload

---
+++ printah2-KaliLinux.py
@@ -0,0 +1,152 @@
+#printah2-KaliLinux.py
+import os, sys, time
+os.system('clear')
+def main (kata):
+        for e in kata:
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.2)
+print ("""
+\033[1;91m       _     ___ _   _ _   ___  __
+      | |   |_ _| \ | | | | \ \/ /
+      | |    | ||  \| | | | |\  / 
+\033[1;37m      | |___ | || |\  | |_| |/  \ 
+      |_____|___|_| \_|\___//_/\_\\
+                \033[1;33mversi \033[1;92m9.9
+""")
+print ("\033[1;91m[\033[1;37m=======================================\033[1;91m]")
+main("""
+        \033[1;37mSelamat datang Gaess \033[1;92m>_
+
+   \033[1;37mSemoga bermanfaat buat kalian \033[1;92m>_<
+
+""")
+print ("\033[1;91m[\033[1;37m=======================================\033[1;91m]")
+nama = input('\033[1;34m( \033[1;33mSTAR \033[1;34m) \033[1;92m--> \033[1;91mEnter \033[1;92m: ')
+time.sleep(3)
+print ('\n')
+main('  \033[1;92mLoading\033[1;37m....')
+time.sleep(4)
+os.system('clear')
+print ("""
+       \033[1;91m(\033[1;33mAuthor\033[1;91m) \033[1;92m: \033[1;37mM_aref
+
+       \033[1;34m(\033[1;37mYoutub\033[1;34m) \033[1;92m: \033[1;33mThe-X-Cyber
+""")
+main("""
+\033[1;37m1 Siapa yang tidak kenal linux?
+
+2 Tidak Semua Perintah Dasar Linux Sama
+
+3 List Perintah Dasar Linux Yang Wajib Diketahui
+
+3.1 man <perintah> untuk meilhat panduan
+
+3.2 <perintah> –help untuk panduan ringkas
+
+3.3 sudo untuk super user
+
+3.4 ls untuk melihat direktori
+
+3.5 cd untuk masuk ke direktori
+
+3.6 mkdir <nama folder> untuk membuat folder
+
+3.7 pwd untuk melihat direktori aktif
+
+3.8 vim untuk membuka text editor
+
+3.9 cp <asal> <tujuan> untuk menyalin file
+
+3.10 mv <asal> <tujuan> untuk memindahkan folder
+
+3.11 rm <file> untuk menghapus file
+
+3.12 find <nama file> untuk mencari file
+
+3.13 history untuk melihat riwayat
+
+3.14 cat untuk melihat isi file
+
+3.15 echo untuk menampilkan baris teks
+
+3.16 grep untuk mencari kata
+
+3.17 wc untuk menampilkan baris baru
+
+3.18 sort untuk mengurutkan
+
+3.19 chmod untuk mengganti hak akses
+
+3.20 chown mengganti hak milik
+
+3.21 su untuk mengganti user id
+
+3.22 passwd untuk mengganti password
+
+3.23 who untuk menampilkan user
+
+3.24 ps untuk menampilkan snapshot
+
+3.25 kill untuk menghentikan program
+
+3.26 tar untuk mengumpulkan file
+
+3.27 zip untuk mengkompres file
+
+3.28 unzip untuk mengekstrak file
+
+3.29 ssh untuk akses jarak jauh
+
+3.30 scp untuk menyalin file
+
+3.31 fdisk untuk menampilkan partisi
+
+3.32 mount untuk melampirkan file
+
+3.33 umount untuk melakukan unmount
+
+3.34 du untuk menampilkan ukuran file
+
+3.35 df untuk menampilkan disk space
+
+3.36 quota menampilkan sisa disk space
+
+3.37 reboot untuk mulai ulang
+
+3.38 poweroff untuk mematikan
+
+3.39 gedit untuk membuka editor teks
+
+3.40 kate untuk membuka teks editor
+
+3.41 bg membuat proses background
+
+3.42 fg <id program> membuat proses foreground
+
+3.43 jobs <id program> menampilkan identitas proses
+
+3.44 sed untuk melakukan filter teks
+
+3.45 awk untuk memindahkan teks
+
+3.46 locate untuk mencari file
+
+3.47 ifconfig untuk melihat ip
+
+3.48 date untuk menampilkan tanggal
+
+3.49 nano merubah teks editor
+
+3.50 top melihat proses secara urut
+
+3.51 clear membersihkan terminal
+
+3.52 dpkg -i <namapackage>.deb instalasi paket
+
+3.53 uname melihat versi kernel
+
+3.54 * untuk mencantumkan deskripsi
+""")
+time.sleep(4)
+os.system("python3 printah2-KaliLinux.py")
